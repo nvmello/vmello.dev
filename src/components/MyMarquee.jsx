59de015ed523785b9b22a421e1b16c3e80219e9d
Nick <nickvmorello@gmail.com>
2024-11-04 23:45:04 -0700

    started on the navbar, i am using react-fast-marquee and got my On Repeat section functioning using the Spotify API

---
+++ src/components/MyMarquee.jsx
@@ -0,0 +1,22 @@
+import Marquee from "react-fast-marquee";
+import Spotify from "./Spotify";
+
+function MyMarquee() {
+  return (
+    <Marquee
+      className="w-4/6 h-16 flex items-center overflow-hidden"
+      pauseOnHover
+      gradient
+      gradientColor="black"
+      speed={30}
+    >
+      <div className="flex items-center gap-32">
+        <Spotify />
+        <h1 className="text-sm whitespace-nowrap">Something else</h1>
+        <h1 className="text-sm whitespace-nowrap">Another one</h1>
+      </div>
+    </Marquee>
+  );
+}
+
+export default MyMarquee;
