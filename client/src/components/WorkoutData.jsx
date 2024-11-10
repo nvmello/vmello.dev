24f5acdf65ade0f77f768d31a443c3a9667cc777
Nick <nickvmorello@gmail.com>
2024-11-10 15:22:04 -0700

    temporary commit

--- client/src/components/WorkoutData.jsx
+++ client/src/components/WorkoutData.jsx
@@ -5,20 +5,34 @@ function WorkoutData() {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("http://192.168.12.88:3000/api/workout")
-      .then((response) => {
+    const fetchWorkout = async () => {
+      try {
+        const response = await fetch("http://192.168.12.113:3000/api/workouts");
+        console.log("Response status:", response.status);
+
         if (!response.ok) {
-          throw new Error("Network response was not ok");
+          throw new Error(
+            `Network response was not ok. Status: ${response.status}`
+          );
         }
-        return response.json();
-      })
-      .then((data) => {
-        setWorkout(data);
-      })
-      .catch((error) => {
+
+        const data = await response.json();
+
+        if (data && data.length > 0) {
+          const latestWorkout = data[0];
+          setWorkout(latestWorkout);
+        } else {
+          setError("No workout data found");
+        }
+      } catch (error) {
         setError(error.message);
         console.error("Error fetching workout data:", error);
-      });
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchWorkout();
   }, []);
 
   if (error) {
@@ -31,9 +45,7 @@ function WorkoutData() {
 
   return (
     <span className="text-sm whitespace-nowrap mr-8">
-      <span className="font-bold">Todays workout:</span>{" "}
-      {Math.round(workout.duration)} minute {workout.type} : Calories Burned:{" "}
-      {Math.round(workout.calories)}{" "}
+      <span className="font-bold">Today's workout:</span> {workout.type}
     </span>
   );
 }
