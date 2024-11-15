a816d4346d2c8eed811b296210b3d1afd5768d3c
Nick <nickvmorello@gmail.com>
2024-11-14 20:54:15 -0700

    initial connections section, still needs some work but i want to push it live to show off this weekend.

---
+++ client/src/components/ConnectionCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const ConnectionCard = ({ data }) => {
+  const { account, icon, username, subSection, subSectionData, link } = data;
+  return (
+    <div className="px-4 w-1/2">
+      <a
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="block w-full"
+        style={{ textDecoration: "none", color: "inherit" }}
+      >
+        <div className="flex flex-col items-center pb-5 bg-black shadow-md shadow-gray-700 hover:shadow-accent-hover transition-shadow duration-500 ease-in-out rounded-lg">
+          <h2 className="text-lg font-bold mt-4">{account}</h2>
+          <i className={`${icon} text-5xl my-2`} />
+          <p className="text-3xl font-bold">{username}</p>
+          <p className="capitalize text-l">{subSection}</p>
+          <p className="capitalize text-sm">{subSectionData}</p>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default ConnectionCard;
--- client/src/components/SectionThree.jsx
+++ client/src/components/SectionThree.jsx
@@ -1,28 +1,92 @@
 import { Network } from "lucide-react";
 import { SectionHeader, SectionContent } from "./layout-components";
+import { ConnectionCard } from "./ConnectionCard";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
 function SectionThree() {
   return (
     <>
       <SectionHeader icon={Network} title="Let's Connect" />
       <SectionContent>
-        {/* Example of extending the base styles for a specific section */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          <p className="text-accent">Coming soon</p>
-          {/* <div className="flex items-center space-x-2 text-accent p-4 rounded-lg hover:bg-accent/5 transition-colors">
-            <p>LinkedIn</p>
-          </div>
-          <div className="flex items-center space-x-2 text-accent p-4 rounded-lg hover:bg-accent/5 transition-colors">
-            <p>GitHub</p>
-          </div>
-          <div className="flex items-center space-x-2 text-accent p-4 rounded-lg hover:bg-accent/5 transition-colors">
-            <p>Twitter</p>
-          </div> */}
-          {/* Add more social links as needed */}
-        </div>
+        <Carousel className=" p-5" responsive={responsive}>
+          <ConnectionCard data={instagramData}></ConnectionCard>
+          <ConnectionCard data={linkedInData}></ConnectionCard>
+          <ConnectionCard data={githubData}></ConnectionCard>
+          <ConnectionCard data={snapchatData}></ConnectionCard>
+          <ConnectionCard data={paypalData}></ConnectionCard>
+          <ConnectionCard data={steamData}></ConnectionCard>
+        </Carousel>
       </SectionContent>
     </>
   );
 }
+
+const steamData = {
+  account: "Steam",
+  icon: "fa-brands fa-steam",
+  // username: "vmello",
+  // subSection: "Some favorites:",
+  // subSectionData: "Valheim, Apex, Elder Scrolls",
+  link: "https://steamcommunity.com/profiles/76561199084454580",
+};
+
+const instagramData = {
+  account: "Instagram",
+  icon: "fa-brands fa-instagram",
+  // username: "nickvmorello",
+  // subSection: "Some favorites:",
+  // subSectionData: "Valheim, Apex, Elder Scrolls",
+  link: "https://www.instagram.com/nickvmorello/",
+};
+
+const linkedInData = {
+  account: "LinkedIn",
+  icon: "fa-brands fa-linkedin",
+  // username: "nicholas@vmello.dev",
+  // subSection: "Some favorites:",
+  // subSectionData: "Valheim, Apex, Elder Scrolls",
+  link: "https://www.linkedin.com/in/nvmello/",
+};
+
+const githubData = {
+  account: "Github",
+  icon: "fa-brands fa-github",
+
+  link: "https://github.com/nvmello",
+};
+
+const paypalData = {
+  account: "Paypal",
+  icon: "fa-brands fa-paypal",
+
+  link: "https://www.paypal.com/paypalme/nvmore",
+};
+
+const snapchatData = {
+  account: "Snap",
+  icon: "fa-brands fa-snapchat",
+
+  link: "https://www.snapchat.com/add/nvmello",
+};
 
 export default SectionThree;
