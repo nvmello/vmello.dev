eaaaa9e5d02feb0e871f33b181456c30b5de194a
Nick <nickvmorello@gmail.com>
2024-11-25 17:53:46 -0700

    new expiremental Connections section

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  svg: {
+    jsx: true // Required for SVG as components
+  }
   optimizeDeps: {
     force: true,
   },
