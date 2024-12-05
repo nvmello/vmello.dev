41c7e1e0a08a11eac2fe650573a5a649b034cad9
Nick <nickvmorello@gmail.com>
2024-12-05 01:05:02 -0700

    quickly integrated lenis smooth scroll, I plan on building animations around this later

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,51 +7,59 @@ import SectionThree from "./components/body/SectionThree";
 import SectionFour from "./components/body/SectionFour";
 import { Section } from "./components/util/layout-components";
 import { useColorContext } from "./context/ColorContext";
+import { ReactLenis, useLenis } from "lenis/react";
 
 function App() {
   const { colorScheme } = useColorContext();
+
+  const lenis = useLenis(({ scroll }) => {
+    // called every scroll
+  });
+
   return (
-    <div className={`${colorScheme.bg} min-h-screen flex-grow`}>
-      <Navbar />
-      <div className="container mx-auto overflow-y-auto">
-        <hr className="mt-16" />
-        <div id="about">
-          <section className="min-h-[35vh] overflow-y-auto">
-            <Section>
-              <SectionOne />
-            </Section>
-          </section>
+    <ReactLenis root>
+      <div className={`${colorScheme.bg} min-h-screen flex-grow`}>
+        <Navbar />
+        <div className="container mx-auto overflow-y-auto">
+          <hr className="mt-16" />
+          <div id="about">
+            <section className="min-h-[35vh] overflow-y-auto">
+              <Section>
+                <SectionOne />
+              </Section>
+            </section>
+          </div>
+          <hr />
+          <div id="projects">
+            <section className="min-h-[35vh] overflow-y-auto">
+              <Section>
+                <SectionTwo />
+              </Section>
+            </section>
+          </div>
+          <hr />
+          <div id="connect">
+            <section className="min-h-[35vh] overflow-y-auto">
+              <Section>
+                <SectionThree />
+              </Section>
+            </section>
+          </div>
+          <hr />
+          <div id="photography">
+            <section className="min-h-[35vh] overflow-y-auto">
+              <Section>
+                <SectionFour />
+              </Section>
+            </section>
+          </div>
         </div>
         <hr />
-        <div id="projects">
-          <section className="min-h-[35vh] overflow-y-auto">
-            <Section>
-              <SectionTwo />
-            </Section>
-          </section>
-        </div>
-        <hr />
-        <div id="connect">
-          <section className="min-h-[35vh] overflow-y-auto">
-            <Section>
-              <SectionThree />
-            </Section>
-          </section>
-        </div>
-        <hr />
-        <div id="photography">
-          <section className="min-h-[35vh] overflow-y-auto">
-            <Section>
-              <SectionFour />
-            </Section>
-          </section>
+        <div className={colorScheme.bg}>
+          <Footer />
         </div>
       </div>
-      <hr />
-      <div className={colorScheme.bg}>
-        <Footer />
-      </div>
-    </div>
+    </ReactLenis>
   );
 }
 
